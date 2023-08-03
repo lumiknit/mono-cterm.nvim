bf165ab676483fa06cf67ed64cd844d724b32587
lumiknit <aasr4r4@gmail.com>
2023-08-03 11:28:46 +0900

    .

--- lua/lualine/themes/mono-cterm.lua
+++ lua/lualine/themes/mono-cterm.lua
@@ -11,19 +11,26 @@ return {
   },
   normal = {
     a = { fg = t["black"].cterm, bg = t["white"].cterm, bold = true },
-    b = { fg = t["brightwhite"].cterm, bg = t["black"].cterm },
+    b = { fg = t["brightwhite"].cterm, bg = t["brightblack"].cterm },
     c = { fg = t["white"].cterm, bg = t["black"].cterm },
     x = { fg = t["white"].cterm, bg = t["black"].cterm },
-    y = { fg = t["brightwhite"].cterm, bg = t["black"].cterm },
+    y = { fg = t["brightwhite"].cterm, bg = t["brightblack"].cterm },
     z = { fg = t["black"].cterm, bg = t["white"].cterm },
   },
   insert = {
     a = { fg = t["black"].cterm, bg = t["brightwhite"].cterm },
+    z = { fg = t["black"].cterm, bg = t["brightwhite"].cterm },
+  },
+  command = {
+    a = { fg = t["black"].cterm, bg = t["brightgreen"].cterm },
+    z = { fg = t["black"].cterm, bg = t["brightgreen"].cterm },
   },
   replace = {
     a = { fg = t["black"].cterm, bg = t["red"].cterm },
+    z = { fg = t["black"].cterm, bg = t["brightred"].cterm },
   },
   visual = {
     a = { fg = t["black"].cterm, bg = t["magenta"].cterm },
+    z = { fg = t["black"].cterm, bg = t["brightmagenta"].cterm },
   }
 }
